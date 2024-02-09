e85be6160bd19b850e8673fe9c550056ae848ef7
Aditya Samant <adityasamant.learnings@gmail.com>
2024-02-09 11:41:56 +0530

    Junits completed.

--- src/main/java/com/adityasamant/learnings/customers/exception/CustomerNotFoundException.java
+++ src/main/java/com/adityasamant/learnings/customers/exception/CustomerNotFoundException.java
@@ -5,4 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class CustomerNotFoundException extends RuntimeException{
+
+    public CustomerNotFoundException() {
+    }
+
+    public CustomerNotFoundException(String s) {
+        super(s);
+    }
 }
